d9618ed2588b9636bc2ebfefee7b4577ba84f01e
Valery Shevchenko <ar4er.valera@gmail.com>
2022-06-06 16:46:48 +0300

    samdkasmc

--- Model3/main3.py
+++ Model3/main3.py
@@ -4,35 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 '''
+1
+0
+0.001
+1e-10
 1.
-0.
 0.001
 0.1
-1.
-1.
-1.1
-1.
-0.01
 '''
 
 c0_1 = math.sqrt(float(input("c0_1: ")))
 c0_2 = math.sqrt(float(input("c0_2: ")))
 dt = float(input("dt: "))
 omega = float(input("omega: "))
-mu_12 = float(input("mu_12: "))
-e_0 = float(input("e_0: "))
-e_1 = float(input("e_1: "))
-e_2 = float(input("e_2: "))
+omega_r = float(input("omega_r: "))
+omega_0 = float(input("omega_0: "))
 gamma = float(input("gamma: "))
-h = 1.
-omega_r = mu_12 * e_0 / (2. * h)
-omega_0 = math.fabs(e_2 - e_1) / h
-delta = omega - omega_0
 
 
 def itereration(t: float, c_1: complex, c_2: complex):
-    dc_1 = (-omega_r * cmath.exp(1j * delta * t) * c_2 + (-1j * gamma / 2. * c_1)) / 1j
-    dc_2 = (-omega_r * cmath.exp(1j * delta * t) * c_1 + (-1j * gamma / 2. * c_2)) / 1j
+    dc_1 = (-omega_r * cmath.exp(1j * omega * t) * c_2) / 1j
+    dc_2 = (-omega_r * cmath.exp(1j * omega * t) * c_1 + ((omega_0 - 1j * gamma / 2.) * c_2)) / 1j
 
     c_1 = c_1 + dc_1 * dt
     c_2 = c_2 + dc_2 * dt
@@ -56,8 +48,10 @@ def run(n: int):
         prob_list_1.append(prob_1)
         prob_list_2.append(prob_2)
         t += dt
+    plt.title("Probability")
     plt.plot(t_list, prob_list_1)
     plt.plot(t_list, prob_list_2)
+    plt.legend(["Probability on zero level", "Probability on first level"], loc="upper right")
     plt.plot(t_list, np.zeros(len(t_list)))
     plt.show()
 
